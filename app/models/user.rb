4129f51b5113afcfc1b442e9361449472c406e14
Rowan Oulton <rowan.oulton@gmail.com>
2015-04-07 14:35:22 +0200

    Fix typo

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
-  validates :email, presence: true, length: { maxmimum: 255 }
+  validates :email, presence: true, length: { maximum: 255 }
 end
