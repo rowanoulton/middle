7ed0e4a2363004027e8fe13e6c70f2c5afce69aa
Rowan Oulton <rowan.oulton@gmail.com>
2015-04-07 15:07:49 +0200

    Set email uniqueness as case insensitive

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ActiveRecord::Base
   validates :name, presence: true, length: { maximum: 50 }
   validates :email, presence: true, length: { maximum: 255 },
     format: { with: VALID_EMAIL_REGEX },
-    uniqueness: true
+    uniqueness: { case_sensitive: false }
 end
