2d3f8f2359175fbdfcb578567de58938816e4f28
Rowan Oulton <rowan.oulton@gmail.com>
2015-04-07 12:26:02 +0200

    Add welcome email action and templates

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,2 +1,7 @@
 class UserMailer < ApplicationMailer
+  def welcome_email(user)
+    @user = user
+    @url  = 'http://localhost:3000/login'
+    mail(to: @user.email, subject: 'Welcome to Middle')
+  end
 end
