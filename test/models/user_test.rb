60cbf71fa09784eaa3652702ec04ecab858cfddc
Rowan Oulton <rowan.oulton@gmail.com>
2015-04-09 16:21:58 +0200

    Fix user tests Broken by introducing has_secure_password

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserTest < ActiveSupport::TestCase
   def setup
-    @user = User.new(name: 'Example user', email: 'example@example.com')
+    @user = User.new(name: 'Example user', email: 'example@example.com', password: 'foobar', password_confirmation: 'foobar')
   end
 
   test 'should be valid' do
