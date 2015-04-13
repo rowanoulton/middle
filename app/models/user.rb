cd975a209b8b4abb2046fd911d9fb6978f864b6d
Rowan Oulton <rowan.oulton@gmail.com>
2015-04-13 21:27:31 +0200

    Attribute accessor for remember token

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
+  attr_accessor :remember_token
   before_save { email.downcase! }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :name, presence: true, length: { maximum: 50 }
@@ -17,5 +18,10 @@ class User < ActiveRecord::Base
   # Return a random token
   def User.new_token
     SecureRandom.urlsafe_base64
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update_attribute(:remember_digest, User.digest(remember_token))
   end
 end
