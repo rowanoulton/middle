737dbbd439a5b1f77f50b85795e9aa138b330a2b
Rowan Oulton <rowan.oulton@gmail.com>
2015-04-13 21:46:16 +0200

    Improved page title handling

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def full_page_title(page_title = '')
+    base_title = 'Middle'
+    if page_title.empty?
+      base_title
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
