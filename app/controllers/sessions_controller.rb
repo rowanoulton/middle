c4cd5be15dd6919800a3fd33c7cd3ba4199f5e93
Rowan Oulton <rowan.oulton@gmail.com>
2015-04-13 19:25:06 +0200

    Add login logic

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,18 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      # Success
+      redirect_to user
+    else
+      flash.now[:danger] = 'Login details incorrect'
+      render 'new'
+    end
+  end
+
+  def destroy
+  end
 end
